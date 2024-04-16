e0ab73a09248d99bd45c05d695e1a6054f405e31
Jase Banta <jase.banta@cognosante.com>
2024-04-16 15:02:22 -0400

    demo repo

--- Homepage.py
+++ Homepage.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import requests
 
 st.write("Hello World")
+
+st.divider()
+
+a = st.slider("question1", min_value=1, max_value=100, step = 1)
+
+if a > 10:
+    import streamlit as st
+    import numpy as np
+
+    with st.chat_message("user"):
+        st.write("Hello 👋")
+        st.line_chart(np.random.randn(30, 3))
+
+data = requests.get("http://localhost:8000")
+st.write(data.content)
