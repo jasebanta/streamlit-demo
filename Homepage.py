3d58de6e43df4d3bcc0e24f101a97f8c2f1fc523
Jase Banta <jasebanta@gmail.com>
2025-03-10 23:22:38 -0400

    try absolute path?

--- Homepage.py
+++ Homepage.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "backend")))
 from backend.data import SampleData
 
 
