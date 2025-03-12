31dc42498138edb59ecde42a45ed77ca4ae897ff
Jase Banta <jasebanta@gmail.com>
2025-03-12 11:22:13 -0400

    should work

--- Homepage.py
+++ Homepage.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "backend")))
+
 from backend.data import SampleData
 
 
