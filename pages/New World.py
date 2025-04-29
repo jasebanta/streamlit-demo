7eb9a7a77c1a666e8dda998aa060c2fa0e17ba77
Jase Banta <jasebanta@gmail.com>
2025-04-28 20:51:55 -0400

    test

--- pages/New World.py
+++ pages/New World.py
@@ -6,20 +6,17 @@ st.set_page_config(page_title="Streamlit with Google Analytics", layout="wide")
 
 st.title("📊 Streamlit App with Google Analytics")
 
-st.write("This is a simple Streamlit app that includes Google Analytics tracking.")
-
 # --- Inject Google Analytics ---
 GA_MEASUREMENT_ID = "G-5KCQSGDVVY"
 
-ga4_script = f"""
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id={GA_MEASUREMENT_ID}"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){{dataLayer.push(arguments);}}
-  gtag('js', new Date());
-  gtag('config', '{GA_MEASUREMENT_ID}');
-</script>
+ga4_script = """
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5822N93W');</script>
+<!-- End Google Tag Manager -->
 """
 
 # Render the GA script (height and width = 0 so it's invisible)
@@ -28,3 +25,13 @@ components.html(ga4_script, height=0, width=0)
 # --- Main app content ---
 st.write("Scroll down for more content.")
 st.button("Click Me!")
+
+body_script = """
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5822N93W"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+"""
+
+# Render the GA script (height and width = 0 so it's invisible)
+components.html(body_script, height=0, width=0)
--- pages/Whole New World.py
+++ pages/Whole New World.py
@@ -4,16 +4,27 @@ import streamlit.components.v1 as components
 # --- Inject Google Analytics ---
 GA_MEASUREMENT_ID = "G-5KCQSGDVVY"
 
-ga4_script = f"""
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id={GA_MEASUREMENT_ID}"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){{dataLayer.push(arguments);}}
-  gtag('js', new Date());
-  gtag('config', '{GA_MEASUREMENT_ID}');
-</script>
+ga4_script = """
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5822N93W');</script>
+<!-- End Google Tag Manager -->
 """
 
 # Render the GA script (height and width = 0 so it's invisible)
 components.html(ga4_script, height=0, width=0)
+
+st.write("Hello, whole new world!")
+
+body_script = """
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5822N93W"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+"""
+
+# Render the GA script (height and width = 0 so it's invisible)
+components.html(body_script, height=0, width=0)
